c6dc0526162542cec4ab6dae63e27309057032a4
WenboKou <kouwenbocn@163.com>
2022-04-26 00:05:30 +0800

    assign2 almost done, need to debug

--- assignments/serafini_starter/WordLadder/src/wordladder.cpp
+++ assignments/serafini_starter/WordLadder/src/wordladder.cpp
@@ -8,11 +8,68 @@
 #include <iostream>
 #include <string>
 #include "console.h"
+#include "filelib.h"
+#include "lexicon.h"
+#include "queue.h"
+#include "simpio.h"
 
 using namespace std;
 
+void printLadder(Stack<string>& ladder) {
+    while (!ladder.isEmpty()) {
+        cout << ladder.pop() << " ";
+    }
+    cout << endl;
+}
+
 int main() {
     // TODO: Finish the program!
+    string filename = getLine("Dictionary file name?");
+    string word1 = getLine("Word #1 (or Enter to quit)");
+    if (word1 == "") {
+        cout << "Have a nice day!" << endl;
+        return 0;
+    }
+    string word2 = getLine("Word #2 (or Enter to quit)");
+    if (word2 == "") {
+        cout << "Have a nice day!" << endl;
+        return 0;
+    }
+    cout << "word1" << word1 << endl;
+    cout << "word2" << word2 << endl;
+
+    Lexicon dictionary;
+    Lexicon exist_words;
+    dictionary = Lexicon(filename);
+    Queue<Stack<string>> ladders;
+    Stack<string> ladder;
+    string word;
+
+    ladder.push(word1);
+    ladders.enqueue(ladder);
+    while (!ladders.isEmpty()) {
+        ladder = ladders.dequeue();
+        word = ladder.peek();
+        for (int i = 0; i < word.length(); i++) {
+            for (char c = 'a'; c < 'z'; c++) {
+                word[i] = c;
+                if (!dictionary.contains(word)) {continue;}
+                Stack<string> ladder_copy = ladder;
+                if (!exist_words.contains(word)) {
+                    ladder_copy.push(word);
+                    if (word == word2) {
+                        ladders.clear();
+                        printLadder(ladder_copy);
+                        break;
+                    } else {
+                        ladders.enqueue(ladder_copy);
+                        exist_words.add(word);
+                    }
+                }
+            }
+        }
+    }
+
 
 
     cout << "Have a nice day." << endl;
